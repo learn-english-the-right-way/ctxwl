2e6c7a7158f8847ed56f58efc34eec6fbbf4de2c
Sherwood Wang <sherwoodwang@zith.org>
2023-04-18 23:35:16 +0800

    fix the lifecycle of dependencies of application main method

--- src/main/java/org/zith/expr/ctxwl/app/CtxwlApplication.java
+++ src/main/java/org/zith/expr/ctxwl/app/CtxwlApplication.java
@@ -4,6 +4,8 @@ import org.apache.commons.cli.*;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.config.Configurator;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.component.LifeCycle;
 import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
 import org.zith.expr.ctxwl.app.config.AppConfigurator;
 import org.zith.expr.ctxwl.common.close.CombinedAutoCloseable;
@@ -17,6 +19,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class CtxwlApplication {
     private static final String ARG_CONFIG = "config";
@@ -78,6 +82,7 @@ public class CtxwlApplication {
         Optional.ofNullable(arguments.config()).ifPresent(config -> configurator.load(new File(config)));
         var configuration = configurator.configuration();
 
+        Server server;
         try (var closeable = CombinedAutoCloseable.create()) {
             var identityService = closeable.register(
                     IdentityServiceCreator.create(
@@ -91,11 +96,23 @@ public class CtxwlApplication {
                             configuration.core().reading().postgreSql().effectiveConfiguration(),
                             configuration.core().reading().mongoDb().effectiveConfiguration()));
 
-            var server = JettyHttpContainerFactory.createServer(
+            server = JettyHttpContainerFactory.createServer(
                     URI.create(configuration.webApi().effectiveBaseUri()),
                     new CtxwlWebApiApplication(identityService, readingService));
             server.start();
+            var asynchronousClosable = new AtomicReference<CombinedAutoCloseable>();
+            var termination = new CompletableFuture<Void>();
+            server.addEventListener(new LifeCycle.Listener() {
+                @Override
+                public void lifeCycleStopped(LifeCycle event) {
+                    Optional.ofNullable(asynchronousClosable.get()).ifPresent(CombinedAutoCloseable::close);
+                    termination.complete(null);
+                }
+            });
+            asynchronousClosable.set(closeable.transfer());
         }
+
+        server.join();
     }
 
     public static CtxwlApplication create(String[] args) {
